8dea2f6746e81974a170aab44670d7dda9e4fae6
cburono <cburono047@g.educaand.es>
2025-03-17 19:07:01 +0100

    ñ

--- ex_html-css-js.js
+++ ex_html-css-js.js
@@ -1,1 +1,13 @@
 //Escribe aquí tu código JavaScript
+let nombre
+let  direccion
+let telefono
+if (nombre===null  ){
+    alert("REVISA EL FORMULARIO: algunos campos son incorrectos o estan equivocados")
+}
+if( direccion<15){
+    alert("REVISA EL FORMULARIO: algunos campos son incorrectos o estan equivocados")
+}
+if (telefono!=9 ){
+    alert("REVISA EL FORMULARIO: algunos campos son incorrectos o estan equivocados")
+}
